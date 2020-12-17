073ce655c19c15f5383e65b144ae61d2dad045c9
noeyhuy <iamhyeon2@naver.com>
2020-12-17 13:55:05 +0900

    Complete Survey

--- Project_root/Survey/urls.py
+++ Project_root/Survey/urls.py
@@ -14,13 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from polls import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls/', views.index, name='index'),
-    path('polls/<int:question_id>/', views.detail, name='detail'),
-    path('polls/<int:question_id>/results/', views.results, name='results'),
-    path('polls/<int:question_id>/vote/', views.vote, name='vote'),
+    path('polls/', include('polls.urls')),
 ]
